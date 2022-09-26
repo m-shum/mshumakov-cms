43d311b5511996c4115202db73eab8beb9493824
Michael <michael.shumakov@gmail.com>
2022-09-26 11:10:53 -0700

    build: add info node

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,16 +1,19 @@
-// First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
-// Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import contactLinks from "./objects/contactLinks";
+import project from "./documents/project";
+import projectsArray from "./documents/projectsArray";
+import projectReference from "./objects/projectReference";
+import portableText from "./objects/portableText";
 
-// Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  // We name our schema
-  name: 'default',
-  // Then proceed to concatenate our document type
-  // to the ones provided by any plugins that are installed
+  name: "default",
   types: schemaTypes.concat([
-    /* Your types here! */
+    contactLinks,
+    project,
+    projectsArray,
+    projectReference,
+    portableText,
   ]),
-})
+});
