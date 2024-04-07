1770cee8f8043b8c2994760523137ab30c9e5c88
Michael Shumakov <62724405+m-shum@users.noreply.github.com>
2024-04-06 17:53:28 -0700

    feat: update sanity cli config
    fix: update cli config

--- sanity.cli.js
+++ sanity.cli.js
@@ -2,7 +2,7 @@ import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: '<your-project-id>', // replace value with your own
-    dataset: '<your-dataset-name>', // replace value with your own
+    projectId: 'nzcunbcl',
+    dataset: 'production',
   },
 })
