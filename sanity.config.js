ab3c02710d8db503ec2f6c56371a98636ff40ac2
Michael Shumakov <62724405+m-shum@users.noreply.github.com>
2024-04-07 09:02:32 -0700

    fix: update sanity config with relevant plugins

--- sanity.config.js
+++ sanity.config.js
@@ -1,26 +1,27 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
 import { visionTool } from '@sanity/vision'
-import { colorInput } from '@sanity/color-input'
-import { schemaTypes } from './schemas'
-import deskStructure from './deskStructure'
 import { dashboardTool } from '@sanity/dashboard'
+import { schemaTypes } from './schemas'
+import { colorInput } from '@sanity/color-input'
 import { media } from 'sanity-plugin-media'
-import { muxInput } from 'sanity-plugin-mux-input'
+import deskStructure from './deskStructure'
 
 export default defineConfig({
+  name: 'portfolio-cms',
   title: 'portfolio-cms',
+
   projectId: 'nzcunbcl',
   dataset: 'production',
+
   plugins: [
     structureTool({
       structure: deskStructure,
     }),
     visionTool(),
+    dashboardTool(),
     colorInput(),
-    dashboardTool({}),
     media(),
-    muxInput(),
   ],
   tools: (prev) => {
     // 👇 Uses environment variables set by Vite in development mode
@@ -30,6 +31,6 @@ export default defineConfig({
     return prev.filter((tool) => tool.name !== 'vision')
   },
   schema: {
-    types: [schemaTypes],
+    types: schemaTypes,
   },
 })
