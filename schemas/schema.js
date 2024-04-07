d405b0ac71fa90c65a9bb9ac770c65548ff8af84
Michael Shumakov <62724405+m-shum@users.noreply.github.com>
2024-04-06 17:01:31 -0700

    refactor: migrate schema

--- schemas/schema.js
+++ schemas/schema.js
@@ -1,21 +1,15 @@
-import schemaTypes from "all:part:@sanity/base/schema-type";
-import createSchema from "part:@sanity/base/schema-creator";
+import info from './documents/info'
+import project from './documents/project'
+import projectsArray from './documents/projectsArray'
+import contactLinks from './objects/contactLinks'
+import portableText from './objects/portableText'
+import projectReference from './objects/projectReference'
 
-import info from "./documents/info";
-import project from "./documents/project";
-import projectsArray from "./documents/projectsArray";
-import contactLinks from "./objects/contactLinks";
-import portableText from "./objects/portableText";
-import projectReference from "./objects/projectReference";
-
-export default createSchema({
-  name: "default",
-  types: schemaTypes.concat([
-    contactLinks,
-    info,
-    project,
-    projectsArray,
-    projectReference,
-    portableText,
-  ]),
-});
+export default [
+  contactLinks,
+  info,
+  project,
+  projectsArray,
+  projectReference,
+  portableText,
+]
