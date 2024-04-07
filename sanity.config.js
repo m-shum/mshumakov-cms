a096c27fe16245754a73578946eb4350a691357a
Michael Shumakov <62724405+m-shum@users.noreply.github.com>
2024-04-06 17:16:13 -0700

    refactor: migrate sanity vision

--- sanity.config.js
+++ sanity.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'sanity'
 import { structureTool } from 'sanity/structure'
+import { visionTool } from '@sanity/vision'
 import schemas from './schemas/schema'
 import deskStructure from './deskStructure'
 
@@ -11,7 +12,15 @@ export default defineConfig({
     structureTool({
       structure: deskStructure,
     }),
+    visionTool(),
   ],
+  tools: (prev) => {
+    // 👇 Uses environment variables set by Vite in development mode
+    if (import.meta.env.DEV) {
+      return prev
+    }
+    return prev.filter((tool) => tool.name !== 'vision')
+  },
   schema: {
     types: [schemas],
   },
